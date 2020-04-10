b6965b7efb11e625c1f48ea1e51db57ff4ed3fa6
ashrafyawar <m.ashraf0016@gmail.com>
2020-04-10 11:32:14 +0300

    first commit

--- AirLineSystem/AirLineSystem.java
+++
@@ -1,5 +0,0 @@
-class AirLineSystem{  
-    public static void main(String args[]){  
-     System.out.println("let's make our own Airport System :)");  
-    }  
-} 
---
+++ AirLineSystem/AirPortSystem.java
@@ -0,0 +1,6 @@
+  
+}class AirPortSystem{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}
