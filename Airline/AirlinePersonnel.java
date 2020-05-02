600cd78778f3875c745130dc171249940c0136fc
Nevzat Seferoglu <nevzat.seferoglu2017@gtu.edu.tr>
2020-05-02 09:45:10 +0300

    Airline backbone

--- Airline/AirlinePersonnel.java
+++
@@ -1,9 +0,0 @@
-package Airline;
-
-/**
- * Backbone of airline personnel .
- * In case of needed  other personnel type can be derived.
- * @author Nevzat Seferoglu
- * @version 1.0
- */
-public class AirlinePersonnel { }
