3023c64055871db0555ebb6b7f0ae0f2a6c943fc
Nevzat Seferoglu <nevzat.seferoglu2017@gtu.edu.tr>
2020-05-02 09:42:01 +0300

    Airline backbone

---
+++ Airline/AirlinePersonnel.java
@@ -0,0 +1,9 @@
+package Airline;
+
+/**
+ * Backbone of airline personnel .
+ * In case of needed  other personnel type can be derived.
+ * @author Nevzat Seferoglu
+ * @version 1.0
+ */
+public class AirlinePersonnel { }
---
+++ Airline/Destination.java
@@ -0,0 +1,12 @@
+package Airline;
+
+
+/**
+ * Destination properties class which is designed
+ * according to perspective of current airport.
+ * @author Nevzat Seferoglu
+ * @version 1.0
+ */
+public class Destination {
+    /* Identifier of airport will be handled */
+}
