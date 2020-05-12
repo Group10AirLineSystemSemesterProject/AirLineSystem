0f25b65b4e89e93cf5561a9eed552bcd962bedab
Rahmet Ali Ölmez <rahmetolmez@gmail.com>
2020-05-12 23:22:09 +0300

    Added lists of classes

--- AirPortSystem/Airport.java
+++ AirPortSystem/Airport.java
@@ -1,9 +1,21 @@
+import Airline.Airline;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Represents airports
  */
 public class Airport
 {
-    // includes instances of customers, companies, shops,
+    // includes instances of customers, airlines, shops,
     // company managers, shop managers and airport admins
+
+    List<Customer> customers = new ArrayList<>();
+    List<Airline> airlines = new ArrayList<>();
+    List<AirportPackage.Shop> shops = new ArrayList<>();
+    List<CompanyManager> companyManagers = new ArrayList<>();
+    List<AirportPackage.ShopManager> shopManagers = new ArrayList<>();
+    List<AirportAdmin> airportAdmins = new ArrayList<>();
 
 }
