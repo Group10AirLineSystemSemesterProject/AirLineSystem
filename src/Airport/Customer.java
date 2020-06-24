ad8636c93e4c33664f5ec5161f565aedae4b2c91
Nevzat Seferoglu <43863606+nevzatseferoglu@users.noreply.github.com>
2020-06-24 17:52:18 +0300

    Add files via upload

--- src/Airport/Customer.java
+++ src/Airport/Customer.java
@@ -1,9 +1,6 @@
 package Airport;
 
-import Airline.Airline;
-import Airline.Flight;
-import Airline.FlightCompareTo;
-import Airline.Ticket;
+import Airline.*;
 import Client.Person;
 import Client.User;
 import Client.UserInterface;
@@ -38,7 +35,8 @@ public class Customer extends User
      * @param airportSystemStorage As current airport to be in.
      * @throws Exception if SSN is already used, or password is not valid.
      * */
-    public Customer( String name , String surname , final String SSN , final String password, final AirportSystemStorage airportSystemStorage ) throws Exception {
+    public Customer( String name , String surname , final String SSN , final String password
+            , final AirportSystemStorage airportSystemStorage ) throws Exception {
 
         super( name , surname );
         if(airportSystemStorage.isValidSSN(SSN)){
@@ -67,44 +65,56 @@ public class Customer extends User
 
         while(loop){
 
-            System.out.println("1- Personal info menu");
-            System.out.println("2- Change ssn");
-            System.out.println("3- Change password.");
-            System.out.println("4- Buy Tickets.");
-            System.out.println("5- See your Tickets.");
-            System.out.println("6- See all shops.");
-            System.out.println("7- Exit.");
+            System.out.printf("1- Personal info menu \n");
+            System.out.printf("2- Change ssn\n");
+            System.out.printf("3- Change password.\n");
+            System.out.printf("4- Buy Tickets.\n");
+            System.out.printf("5- See your Tickets.\n");
+            System.out.printf("6- See all shops.\n");
+            System.out.printf("7- Exit.\n");
 
             choice = in.nextInt();
 
             switch (choice){
                 case 1:
-                    System.out.println(this);
                     break;
                 case 2:
-                    airportSystemStorage.changeSSNwithMenu(in);
                     break;
                 case 3:
-                    airportSystemStorage.changePasswordwithMenu(in);
+                    break;
                 case 4:
                     loop2 = true;
                     while (loop2){
-                        System.out.println("1- See all flights.");
-                        System.out.println("2- Search a flight using PNR.");
-                        System.out.println("3- Buy a ticket.");
-                        System.out.println("4- Exit.");
+                        System.out.printf("1- See all flights.\n");
+                        System.out.printf("2- Search a ticket using PNR.\n");
+                        System.out.printf("3- Buy a ticket.\n");
+                        System.out.printf("4- Exit.\n");
 
                         choice = in.nextInt();
 
                         switch (choice){
                             case 1:
+
+                                for(Map.Entry<String, Airline> ele: airportSystemStorage.getAirlines().entrySet()) {
+                                    System.out.println( "" + ele.getValue().getAirlineSystemStorage().getNameOfTrademarkAsIdentifier()
+                                                    + " Flights List : ");
+                                    for( Flight flight : ele.getValue().getAirlineSystemStorage().getListOfFlight() ) {
+                                        System.out.println( flight.customerShow() );
+                                    }
+                                    System.out.println();
+                                }
+
                                 break;
                             case 2:
                                 break;
+
                             case 3:
                                 break;
-                            case 4: loop2 = false;
+
+                            case 4:
+                                loop2 = false;
                                 break;
+
                             default: System.out.printf("Error. Your input is invalid..\n");
                         }
                     }
@@ -214,7 +224,11 @@ public class Customer extends User
 
     @Override
     public String toString() {
-        return super.toString()+"\nTickets:"+tickets;
+        return "Customer{" +
+                "tickets=" + tickets +
+                ", SSN='" + SSN + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 
     /**Compare method that works with enums,
